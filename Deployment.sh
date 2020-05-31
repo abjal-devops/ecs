c124e3c420a13f2a4b8199ad74c44b21bf755418
abjal-devops <66158379+abjal-devops@users.noreply.github.com>
2020-05-31 01:52:58 -0700

    Update Deployment.sh

--- Deployment.sh
+++ Deployment.sh
@@ -2,8 +2,8 @@
 #sh ecr-login.sh
 docker build -t 470859871030.dkr.ecr.us-east-1.amazonaws.com/test:${BUILD_ID} .
 docker push 470859871030.dkr.ecr.us-east-1.amazonaws.com/test:${BUILD_ID}
-image=470859871030.dkr.ecr.us-east-1.amazonaws.com/test:${BUILD_ID}
-sed -i 's#image_name#'"${image}"'#' TD-nginx.json
+IMAGE=470859871030.dkr.ecr.us-east-1.amazonaws.com/test:${BUILD_ID}
+sed -i 's#image_name#'"${IMAGE}"'#' TD-nginx.json
 aws ecs register-task-definition --family nginx --cli-input-json file://TD-nginx.json --region us-east-1
 #aws ecs create-service --service-name nginx --launch-type EC2 --desired-count 1 --task-definition nginx --cluster test --region us-east-1
 REVISION=`aws ecs describe-task-definition --task-definition nginx --region us-east-1 |jq .taskDefinition.revision`
